fa47b439fdc20eadf3f9561673903b606ccfa011
alkharyeef <40389935+abdullahalkharyeef@users.noreply.github.com>
2020-07-31 23:39:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 
+
+function clickone () {
+document.getElementsByClassName("card")[1].style.display= 'none';
+document.getElementsByClassName("card")[2].style.display= 'none';
+document.getElementsByClassName("card")[3].style.display= 'none';
+}
+function clicktwo () {
+document.getElementsByClassName("card")[0].style.display= 'none';
+document.getElementsByClassName("card")[2].style.display= 'none';
+document.getElementsByClassName("card")[3].style.display= 'none';
+}
+function clickthree () {
+document.getElementsByClassName("card")[0].style.display= 'none';
+document.getElementsByClassName("card")[1].style.display= 'none';
+document.getElementsByClassName("card")[3].style.display= 'none';
+}
+function clickfour () {
+document.getElementsByClassName("card")[0].style.display= 'none';
+document.getElementsByClassName("card")[1].style.display= 'none';
+document.getElementsByClassName("card")[2].style.display= 'none';
+}
