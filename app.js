5eaa75323cfe2cb5b9c0f5508ad9f5b4f20b03f8
alkharyeef <40389935+abdullahalkharyeef@users.noreply.github.com>
2020-08-31 23:46:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1432,7 +1432,7 @@ function answer10 (x) {
     }
     
     function titlereturn (){
-        //   myApp.apptitle=true;
+        myApp.title=true;
         //   myApp.apptitlereturn=false;
         //   myApp1.card1= true;
         myApp1.img1=false;
